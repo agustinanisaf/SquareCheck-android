c45956dec650fc11c21b0790d61e19109ab3abfb
Riza Nafis <riza.nafis@gmail.com>
2020-11-15 14:38:09 +0700

    Create Dosen Rekap Absensi Layout Fixes #10

---
+++ app/src/main/java/com/squarecheck/LecturerAttendanceSummaryActivity.java
@@ -0,0 +1,14 @@
+package com.squarecheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LecturerAttendanceSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lecturer_attendance_summary);
+    }
+}
