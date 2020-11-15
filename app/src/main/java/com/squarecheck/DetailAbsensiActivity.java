80e84dc2d4bf58fcd3a20f12400eccbe47b174df
fadlikamohamad <fadlikamohamad@gmail.com>
2020-11-15 20:55:09 +0700

    Create Mahasiswa Detail Absensi Mata Kuliah Layout Fixes #7

--- app/src/main/java/com/squarecheck/DetailAbsensiActivity.java
+++ app/src/main/java/com/squarecheck/DetailAbsensiActivity.java
@@ -1,8 +1,13 @@
 package com.squarecheck;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailAbsensiActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class DetailAbsensiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_absensi);
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewPresenceList);
+        TableViewAdapter adapter = new TableViewAdapter(getPresenceList());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List getPresenceList() {
+        List presenceList = new ArrayList<>();
+
+        presenceList.add(new PresenceModel("Jumat, 2 Nov 20", "08:03"));
+        presenceList.add(new PresenceModel("Jumat, 9 Nov 20", "08:05"));
+        presenceList.add(new PresenceModel("Jumat, 16 Nov 20", "08:00"));
+        presenceList.add(new PresenceModel("Jumat, 23 Nov 20", "08:02"));
+        presenceList.add(new PresenceModel("Jumat, 30 Nov 20", "08:04"));
+
+        return presenceList;
     }
 }
--- app/src/main/java/com/squarecheck/PresenceModel.java
+++ app/src/main/java/com/squarecheck/PresenceModel.java
@@ -1,4 +1,27 @@
 package com.squarecheck;
 
 public class PresenceModel {
+    private String date;
+    private String time;
+
+    public PresenceModel(String date, String time) {
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
