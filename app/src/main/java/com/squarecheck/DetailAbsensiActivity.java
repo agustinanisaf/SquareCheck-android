667d11ee48ff1bb469f69e578ab49738e82b5880
Riza Nafis <riza.nafis@gmail.com>
2020-11-17 16:10:54 +0700

    Create Pie Chart for Attendance Status Fixes #16

--- app/src/main/java/com/squarecheck/DetailAbsensiActivity.java
+++ app/src/main/java/com/squarecheck/DetailAbsensiActivity.java
@@ -1,10 +1,16 @@
 package com.squarecheck;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +27,32 @@ public class DetailAbsensiActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
+
+        PieChart pieChart = findViewById(R.id.attendance_chart);
+        Log.d("TAG", "onCreate: " + pieChart.getHoleRadius());
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(102.0f, getString(R.string.hadir_label)));
+        entries.add(new PieEntry(12.0f, getString(R.string.izin_label)));
+        entries.add(new PieEntry(23.0f, getString(R.string.terlambat_label)));
+        entries.add(new PieEntry(2.0f, getString(R.string.alpa_label)));
+
+        PieDataSet set = new PieDataSet(entries, "Attendance");
+        set.setColors(new int[]{R.color.hadir, R.color.ijin, R.color.telat, R.color.alpa}, this);
+        set.setDrawValues(false);
+        PieData data = new PieData(set);
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+        pieChart.setDescription(null);
+        pieChart.setCenterText("80%");
+        pieChart.setCenterTextColor(getResources().getColor(R.color.hadir));
+        pieChart.setCenterTextSize(28.f);
+        pieChart.setTransparentCircleRadius(0.0f);
+        pieChart.setHoleRadius(75.f);
+        pieChart.setEntryLabelTextSize(0.0f);
+        pieChart.setDrawEntryLabels(false);
+        pieChart.setDrawMarkers(false);
+        pieChart.getLegend().setEnabled(false);
     }
 
     private List getPresenceList() {
