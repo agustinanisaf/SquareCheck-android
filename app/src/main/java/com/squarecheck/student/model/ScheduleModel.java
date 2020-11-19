27c06de7a0c11d72fa31f17bb69b54a8cba454c7
rizabach <riza.bachtiar.rb@gmail.com>
2020-11-19 08:49:38 +0700

    Membuat model Classroom, Department, Schedule, Subject

--- app/src/main/java/com/squarecheck/student/model/ScheduleModel.java
+++ app/src/main/java/com/squarecheck/student/model/ScheduleModel.java
@@ -1,4 +1,64 @@
 package com.squarecheck.student.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class ScheduleModel {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("subject")
+    @Expose
+    private SubjectModel subject;
+    @SerializedName("time")
+    @Expose
+    private String time;
+    @SerializedName("start_time")
+    @Expose
+    private Object startTime;
+    @SerializedName("end_time")
+    @Expose
+    private Object endTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public SubjectModel getSubject() {
+        return subject;
+    }
+
+    public void setSubject(SubjectModel subject) {
+        this.subject = subject;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Object getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Object startTime) {
+        this.startTime = startTime;
+    }
+
+    public Object getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Object endTime) {
+        this.endTime = endTime;
+    }
+
 }
