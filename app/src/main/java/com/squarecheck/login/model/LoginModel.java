57154ace2c6fa9bbb877d855ba968fdc99f42d2a
fadlikamohamad <fadlikamohamad@gmail.com>
2020-11-19 13:29:27 +0700

    Menambahkan Model dan Presenter untuk Login

--- app/src/main/java/com/squarecheck/login/model/LoginModel.java
+++ app/src/main/java/com/squarecheck/login/model/LoginModel.java
@@ -1,4 +1,16 @@
 package com.squarecheck.login.model;
 
+import com.squarecheck.login.contract.LoginContract;
+
 public class LoginModel {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- app/src/main/java/com/squarecheck/login/presenter/LoginPresenter.java
+++ app/src/main/java/com/squarecheck/login/presenter/LoginPresenter.java
@@ -1,4 +1,18 @@
 package com.squarecheck.login.presenter;
 
-public class LoginPresenter {
+import com.squarecheck.login.contract.LoginContract;
+
+public class LoginPresenter implements LoginContract.Presenter{
+    private LoginContract.View view;
+
+    public LoginPresenter(LoginContract.View view) {
+        this.view = view;
+        this.view.setPresenter(this);
+    }
+
+    public void start() {}
+
+    public void authenticate(String email, String password) {
+
+    }
 }
