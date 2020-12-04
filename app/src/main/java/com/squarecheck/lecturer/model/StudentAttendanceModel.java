0a9d57378923d2e9561a7e4d1455b394316a4e9b
fadlikamohamad <56854569+fadlikamohamad@users.noreply.github.com>
2020-12-04 17:07:35 +0700

    Create Lecturer Attendance Detail Summary (Activity, Fragment, Contract, Presenter, Interactor) & Presence Adapter (#51)
    * Create Lecturer Attendance Detail Summary (Activity, Fragment, Contract, Presenter, Interactor) & Presence Adapter
    Fixes #39
    
    * Create Lecturer Attendance Detail Summary (Activity, Fragment, Contract, Presenter, Interactor) & Presence Adapter
    Fixes #39
    
    * Revert BaseRecyclerAdapter
    Fixes #39
    
    * Revert LecturerDashboardFragment Constant
    Fixes #39
    
    * Fix StudentAttendancesAdapter
    Fixes #39
    
    * Fix Title
    Fixes #39
    
    * Fix LecturerStatsRecycler
    Fixes #39
    
    * Delete PresencesAdapter
    Fixes #39
    
    Co-authored-by: Riza Nafis <riza.nafis@gmail.com>

---
+++ app/src/main/java/com/squarecheck/lecturer/model/StudentAttendanceModel.java
@@ -0,0 +1,67 @@
+package com.squarecheck.lecturer.model;
+
+import com.squarecheck.R;
+import com.squarecheck.student.model.PresenceModel;
+
+public class StudentAttendanceModel {
+    private String status;
+    private String name;
+    private String nrp;
+    private int color;
+
+    public static StudentAttendanceModel fromPresenceModel(PresenceModel presenceModel) {
+        StudentAttendanceModel studentAttendanceModel = new StudentAttendanceModel();
+        // Capitalize Status String
+        String status = presenceModel.getStatus();
+        studentAttendanceModel.status = status.substring(0, 1).toUpperCase() + status.substring(1).toLowerCase();
+        studentAttendanceModel.name = presenceModel.getStudent().getName();
+        studentAttendanceModel.nrp = presenceModel.getStudent().getNrp();
+        switch (presenceModel.getStatus()) {
+            case "hadir":
+                studentAttendanceModel.color = R.color.hadir;
+                break;
+            case "izin":
+                studentAttendanceModel.color = R.color.ijin;
+                break;
+            case "terlambat":
+                studentAttendanceModel.color = R.color.telat;
+                break;
+            case "alpa":
+                studentAttendanceModel.color = R.color.alpa;
+                break;
+        }
+        return studentAttendanceModel;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNrp() {
+        return nrp;
+    }
+
+    public void setNrp(String nrp) {
+        this.nrp = nrp;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
