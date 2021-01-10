0a321c828f703f0a5610a540f5071e0f2dbf26af
Riza Nafis <31271087+ryuuzake@users.noreply.github.com>
2021-01-10 16:29:16 +0700

    Change Localization on Time (#74)
    Fixes #70

--- app/src/main/java/com/squarecheck/shared/util/DateUtil.java
+++ app/src/main/java/com/squarecheck/shared/util/DateUtil.java
@@ -5,23 +5,26 @@ import android.annotation.SuppressLint;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateUtil {
+    private static Locale locale = new Locale("in", "ID");
+
     public static String getDay(Date date) {
         @SuppressLint("SimpleDateFormat")
-        DateFormat dateFormat = new SimpleDateFormat("EEEE");
+        DateFormat dateFormat = new SimpleDateFormat("EEEE", locale);
         return (date != null) ? dateFormat.format(date) : null;
     }
 
     public static String getDate(Date date) {
         @SuppressLint("SimpleDateFormat")
-        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", locale);
         return (date != null) ? dateFormat.format(date) : null;
     }
 
     public static String getFullDate(Date date) {
         @SuppressLint("SimpleDateFormat")
-        DateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM yyyy");
+        DateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM yyyy", locale);
         return (date != null) ? dateFormat.format(date) : null;
     }
 
