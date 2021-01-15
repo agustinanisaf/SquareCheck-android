d07c0c112183701c38faa26cee0d4657ee28a442
Riza Nafis <31271087+ryuuzake@users.noreply.github.com>
2021-01-15 22:14:13 +0700

    Aanmfa/issue71 (#78)
    * Create About Us Layout (#71)
    
    * Show Splash Screen And About Us (#71)
    
    * Show Splash Screen And About Us (#71)
    
    * Show Splash Screen And About Us (#71)
    
    * Fix Showing Image with Glide
    Fixes #71
    
    * Fix RecyclerView
    Fixes #71
    
    Co-authored-by: AanMFA <aanmfa06@gmail.com>

---
+++ app/src/main/java/com/squarecheck/about/AboutUsActivity.java
@@ -0,0 +1,44 @@
+package com.squarecheck.about;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squarecheck.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+        initializeDeveloperList();
+        initializeSupervisorsMentors();
+    }
+
+    private void initializeSupervisorsMentors() {
+        RecyclerView recyclerView = findViewById(R.id.rv_supervisors);
+        String[] nameList = getResources().getStringArray(R.array.supervisors_mentors_names);
+        RecyclerVAdapterSupervisors adapter = new RecyclerVAdapterSupervisors(nameList);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void initializeDeveloperList() {
+        RecyclerView recyclerView = findViewById(R.id.rv_developers);
+        int[] profileImage = {
+                R.drawable.developer_agustin, R.drawable.developer_fadli,
+                R.drawable.developer_nafis, R.drawable.developer_rizbach,
+                R.drawable.developer_fahreza
+        };
+        String[] developerNames = getResources().getStringArray(R.array.developer_names);
+        RecyclerVAdapterDeveloper adapter = new RecyclerVAdapterDeveloper(developerNames, profileImage);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+}
